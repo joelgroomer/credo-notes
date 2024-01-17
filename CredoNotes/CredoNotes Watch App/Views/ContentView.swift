34885d28ec9fc9b4679448d372d00445d9319c1a
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-16 21:02:28 -0800

    Layout ContentView

--- CredoNotes/CredoNotes Watch App/Views/ContentView.swift
+++ CredoNotes/CredoNotes Watch App/Views/ContentView.swift
@@ -8,17 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+    
+    // MARK: - FUNCTIONS
+    func save() {
+        dump(notes)
+    }
+    
+    
+    // MARK: - BODY
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack(alignment: .center, spacing: 6) {
+                TextField("Add New Note", text: $text)
+                
+                Button {
+                    // 1. Only run if text field is not emtpy
+                    guard text.isEmpty == false else { return }
+                    
+                    // 2. Create a new note item with text value
+                    let note = Note(id: UUID(), text: text)
+                    
+                    // 3. Append to notes array
+                    notes.append(note)
+                    
+                    // 4. Clear text field
+                    text = ""
+                    
+                    // 5. Save the notes
+                    save()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 42, weight: .semibold))
+                }
+                .fixedSize()
+                .buttonStyle(.plain)
+                .foregroundStyle(.accent)
+            } //: HStack
+            
+            Spacer()
+            
+            Text("\(notes.count)")
+        } //: VStack
+        .navigationTitle("Notes")
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     ContentView()
 }
