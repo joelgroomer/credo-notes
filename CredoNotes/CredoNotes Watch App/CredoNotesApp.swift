fd87e83eb5ad1c567755678b27c702e63d37b416
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-16 20:34:29 -0800

    Create new project

---
+++ CredoNotes/CredoNotes Watch App/CredoNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CredoNotesApp.swift
+//  CredoNotes Watch App
+//
+//  Created by Joel Groomer on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CredoNotes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
